2aaf9084b4998711acaece3fbae5bb7c760baddc
menachem yarhi <nbjo460@gmail.com>
2025-05-12 12:48:51 +0300

    added signs funcs 1. create_statics 2. validate_numbers

--- Project.cs
+++ Project.cs
@@ -8,11 +8,28 @@ namespace Tutorial
 {
     internal class Project
     {
+
+        static void create_statics(string _numbers)
+        {
+            string[] numbers = _numbers.Split(" ");
+
+            if (validate_numbers(numbers))
+            {
+
+            }
+        }
+
+        static bool validate_numbers(string[]_numbers)
+        {
+            return true;
+        }
         static void Main(string[] args)
         {
 
-            string d = Console.ReadLine();
-            Console.WriteLine(d);
+            string numbers= Console.ReadLine();
+            create_statics(numbers);
+
+
 
 
             while (true)
