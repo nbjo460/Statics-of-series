5582052eb390c6af49ae457e5c523e752a9d4da7
menachem yarhi <nbjo460@gmail.com>
2025-05-12 16:03:32 +0300

    added reques_input()

--- Project.cs
+++ Project.cs
@@ -14,16 +14,35 @@ namespace Tutorial
             Dictionary<int, object> dict = new Dictionary<int, object>();
             List<int> numbers = new List<int>();
 
-            dict.Add(0, dict);
+            dict.Add(0, "string numbers");
             dict.Add(1, numbers);
 
+            request_input(dict);
 
 
         }
 
 
+        static void request_input(Dictionary<int, object> _dict)
+        {
+           string numbers;
+            while (true)
+            {
+                numbers = Console.ReadLine();
+                string indication = validation_number(_dict);
+                if (indication == "excellent")
+                {
+                    _dict[0] = numbers;
+                    return;
+                }
+                Console.WriteLine(indication);
+            }
 
-
+        }
+        static string validation_number(Dictionary<int, object> _dict)
+        {
+            return "";
+        }
 
 
 
