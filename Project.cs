49f6970199fe9c588b96a00154a34c6cc2801bae
menachem yarhi <nbjo460@gmail.com>
2025-05-12 16:38:32 +0300

    added convert_to_list()

--- Project.cs
+++ Project.cs
@@ -42,11 +42,53 @@ namespace Tutorial
         static string validation_number(Dictionary<int, object> _dict)
         {
             convert_to_list(_dict);
-
-            return "";
+            List<int> numbers = (List<int>)_dict[1];
+            
+            if (numbers.Count() < 3) 
+            {
+                return "You gave less then 3 numbers.\nTry again.";
+            }
+            foreach (int num in numbers)
+            {
+                if (num < 0)
+                {
+                    return $"{num} is an negative number. Try again. ONLY POSITIVE NUMERS.";
+                }
+            }
+            return "excellent";
         }
         static void convert_to_list(Dictionary<int, object> _dict)
         {
+            List<int> numbers = new List<int>();
+            string str_num = (string) _dict[0];
+            string[] arr_nums = str_num.Split(" ");
+
+            foreach (string num in arr_nums)
+            {
+                int num_int;
+                try 
+                {
+                    num_int = Convert.ToInt32(num);
+                    numbers.Add(num_int);
+                }
+                catch //if contains leeters or special symbols
+                {
+                    string decompision_num = "";
+                    foreach (char c in num) //ignore of non digits. but may be without digits.
+                    {
+                        if (Char.IsDigit(c))
+                        {
+                            decompision_num += c;
+                        }
+                    }
+                    if (decompision_num.Length > 0) //validate, that it's will include digits
+                    {
+                        num_int = Convert.ToInt32(decompision_num);
+                        numbers.Add(num_int);
+                    }
+                }
+            }
+
 
         }
 
